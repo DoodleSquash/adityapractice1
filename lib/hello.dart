d93481438da00e4afdc02c5070ec094b2328da5b
DoodleSquash <doodlesquash2006@gmail.com>
2025-01-22 19:40:46 +0530

    Create hello.dart

---
+++ lib/hello.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class hey extends StatefulWidget {
+  const hey({super.key});
+
+  @override
+  State<hey> createState() => _heyState();
+}
+
+class _heyState extends State<hey> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
